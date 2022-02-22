200b15e6cf463b21e64569ad5361d05dd2ae9b39
Luis Fernando <luis.silva9902@gmail.com>
2022-02-22 15:07:25 -0300

    Second complete challenge.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,48 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard'
+import { Header } from './Header'
+import { useEffect, useState } from 'react';
+import { api } from '../services/api';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selectedGenreId: number,
+  selectedGenre: {
+    title: string
+  }
+}
+
+export function Content({ selectedGenreId, selectedGenre }: ContentProps) {
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+  }, [selectedGenreId]);
+
+  return (
+    <div className="container">
+
+        <Header title={selectedGenre.title}/>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
